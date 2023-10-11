fb68bc966bc620685f5eb3217a0838b492119af1
Natalie Childs <nata.faye@gmail.com>
2023-10-11 15:25:36 -0700

    Fetching from lego and from movie API

--- using-an-api.js
+++ using-an-api.js
@@ -1,21 +1,25 @@
 
 /***** LEGO *****/
 
-const onFetchLegoClick = () => {
-    const data = fetchLegoColors()
+const onFetchLegoClick = async () => {
+    const data = await fetchLegoColors()
     renderLegoColors(data)
 }
 
-function fetchLegoColors() {
-    // TODO: get the lego colors from the API
-    return []
+async function fetchLegoColors() {
+    const response = await fetch("https://rebrickable.com/api/v3/lego/colors/?page_size=10&key=" + API_KEY)
+    const data = await response.json()
+    return data.results
 }
 
 // Renders the lego colors in spans with the color as the background color
 const legoContainer = document.getElementById("lego-container")
 function renderLegoColors(data) {
-    legoContainer.innerHTML = data.results.map(color => `
-        <span class="text-white p-1 m-1 d-inline-block" style="background-color: #${color.rgb}">
+    legoContainer.innerHTML = data.map(color => `
+        <span 
+            class="text-white p-1 m-1 d-inline-block" 
+            style="background-color: #${color.rgb}"
+        >
             ${color.name}
         </span>`
     ).join("") 
@@ -24,22 +28,23 @@ function renderLegoColors(data) {
 
 /***** MOVIES *****/
 
-function onFetchMovieClick() {
-    const data = fetchMovie()
+async function onFetchMovieClick() {
+    const data = await fetchMovie()
     renderMovie(data)
 }
 
-function fetchMovie() {
-    // TODO: get the movie from the API
-    return {}
+async function fetchMovie() {
+    const response = await fetch(`http://www.omdbapi.com/?apikey=${MOVIE_API_KEY}&type=movie&t=Empire+Strikes+Back`)
+    const data = await response.json()
+    return data
 }
 
 // Renders the movies as a div with the title and the plot
 const movieContainer = document.getElementById("movie-container")
 function renderMovie(movie) {
-    console.log(movie)
     movieContainer.innerHTML = `
         <div>
+            <img class="img-thumbnail" src="${movie.Poster}"/>
             <h5>${movie.Title}</h5>
             <p>${movie.Plot}</p>
         </div>
@@ -67,7 +72,10 @@ const pokemonContainer = document.getElementById("pokemon-container")
 function renderPokemon(pokemonData) {
     pokemonContainer.innerHTML = `
         <div class="text-center">
-            <img class="img-thumbnail" src="${pokemonData.sprites.front_default}"/>
+            <img 
+                class="img-thumbnail" 
+                src="${pokemonData.sprites.front_default}"
+            />
             <h5 class="mt-3">${pokemonData.name}</h5>
             <p>Base Experience: ${pokemonData.base_experience}</p>
         </div>
